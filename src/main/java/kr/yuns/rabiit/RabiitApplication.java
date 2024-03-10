19e0bc419fa0a34452573c232b86176bf16a6d3e
Yun-Hwan Jeon <yunsol267@gmail.com>
2024-03-10 11:55:50 +0900

    setting: initialize project

---
+++ src/main/java/kr/yuns/rabiit/RabiitApplication.java
@@ -0,0 +1,13 @@
+package kr.yuns.rabiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabiitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabiitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/yuns/rabiit/RabiitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yuns.rabiit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabiitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
